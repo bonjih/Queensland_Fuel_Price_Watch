d1c4a57312abd3dff678e6e49e9270f269b30d65
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-09-17 12:46:14 +1000

    removed sql dup check

--- db_manager.py
+++ db_manager.py
@@ -12,19 +12,18 @@ class SQL:
         self.engine = create_engine(f"mysql+pymysql://{user}:{pwd}@{host}/{db}?charset={'latin1'}")
 
     def insert_dataframe(self, df, table_name):
-        """Insert a DataFrame into the specified table. Append for 'sites_prices' and replace for other tables."""
+        """Insert a DataFrame into the specified table. Append for 'sites_prices' and replace for others."""
         try:
             # Add 'updated' column with the current datetime
             df['updated'] = datetime.now()
 
-            # Check if the table is 'sites_prices' and append, else replace
+            # Special handling for 'sites_prices' table
             if table_name == 'sites_prices':
-                if_exists_option = 'append'
+                if_exists = 'append'  # Append for sites_prices table
             else:
-                if_exists_option = 'replace'  # This will delete all rows and replace with new data
+                if_exists = 'replace'  # Replace for other tables
 
-            # Insert data into the table
-            df.to_sql(table_name, con=self.engine, if_exists=if_exists_option, index=False)
-            print(f"Data inserted into table '{table_name}' with {if_exists_option} mode.")
+            df.to_sql(table_name, con=self.engine, if_exists=if_exists, index=False)
+            print(f"Data inserted into table '{table_name}' with {if_exists} mode.")
         except Exception as e:
             print(f"Failed to insert data into table '{table_name}': {str(e)}")
