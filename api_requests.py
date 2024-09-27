546ca1b94dc770b3b029b5332e18e2ac6e9d3aeb
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-09-28 06:57:00 +1000

    added brent crude API

--- api_requests.py
+++ api_requests.py
@@ -12,3 +12,16 @@ def fetch_currency_data(token, start_date, end_date):
         return response.json()
     else:
         raise Exception(f"API request failed with status code {response.status_code}: {response.text}")
+
+
+# call brent crude API
+def fetch_brent_data(token, period):
+    url = f"https://api.eia.gov/v2/petroleum/pri/spt/data/?api_key={token}&" \
+          f"frequency=daily&data[0]=value&facets[series][]=RBRTE&sort[0][column]=period&sort[0][direction]=desc&" \
+          f"offset=0&length={period}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"API request failed with status code {response.status_code}: {response.text}")
